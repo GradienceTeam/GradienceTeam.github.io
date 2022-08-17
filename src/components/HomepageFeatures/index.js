e688ed59e437786b2e28846d40021d03780f9e00
0xMRTT <0xMRTT@tuta.io>
2022-08-17 14:58:39 +0200

    feat: add features

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,28 +8,27 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Gradience offer a simple way to customize
+        any colours of Adwaita.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Material Color',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        With Gradience, you can generate your own preset from your background using Google Material Color 3.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'More than Adwaita',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Extend Gradience with plugins and customize GDM, Firefox Gnome, GNOME Shell...
+        You can also create your own plugin for customizing everything you want.
       </>
     ),
   },
