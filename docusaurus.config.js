5d762959f7c1a3cee1ef7024cf900939f27983a4
0xMRTT <0xMRTT@tuta.io>
2022-09-05 19:18:01 +0200

    fix: edit url
    Signed-off-by: 0xMRTT <0xMRTT@tuta.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,14 +39,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GradienceTeam/GradienceTeam.github.io/tree/main/docs',
+            'https://github.com/GradienceTeam/GradienceTeam.github.io/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GradienceTeam/GradienceTeam.github.io/tree/main/blog',
+            'https://github.com/GradienceTeam/GradienceTeam.github.io/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
