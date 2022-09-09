7edc2b72fbb9a15a68e7671189d6ba62735270ed
0xMRTT <0xMRTT@tuta.io>
2022-09-09 18:49:52 +0200

    fix: display right pictures depends of color

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import ThemedImage from '@theme/ThemedImage';
 
 import Translate, {translate} from '@docusaurus/Translate';
 
 const FeatureList = [
   {
     title: 'Easy to Use',
+    alt: 'Easy to Use',
     image: require('@site/static/img/welcome.png').default,
+    image_dark: require('@site/static/img/welcome-dark.png').default,
     description: (
       <Translate id="homepage.features.1">
         Gradience offer a simple way to customize
@@ -17,7 +20,9 @@ const FeatureList = [
   },
   {
     title: 'Material Color',
+    alt: 'Material Color',
     image: require('@site/static/img/monet.png').default,
+    image_dark: require('@site/static/img/monet-dark.png').default,
     description: (
       <Translate id="homepage.features.2">
         With Gradience, you can generate your own preset from your background using Material You.
@@ -27,6 +32,8 @@ const FeatureList = [
   {
     title: 'More than Adwaita',
     image: require('@site/static/img/plugins.png').default,
+    image_dark: require('@site/static/img/plugins-dark.png').default,
+    alt: 'More than Adwaita',
     description: (
       <Translate id="homepage.features.3">
         Extend Gradience with plugins and customize GDM, Firefox Gnome, GNOME Shell...
@@ -36,11 +43,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, image, description}) {
+function Feature({title, alt, image, image_dark, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img className={styles.featureSvg} src={image} />
+        <ThemedImage className={styles.featureimage} alt={{alt}} sources={{light: image_dark, dark: image}} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
